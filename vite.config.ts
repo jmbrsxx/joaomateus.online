14392e110dd23b89f465118bada06ba9fc0a6814
João Mateus <jmsrs375@gmail.com>
2026-04-03 15:41:34 -0300

    message

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/joaomateus.online/', // GitHub Pages project repository path
+  base: '/', // GitHub Pages project repository path
 })
